1b5cbe98cc3ea8bad64527af21f9ebb3e41dd260
Prajwal <prajwalpidekar233@gmail.com>
2026-08-27 17:06:09 +0530

    fiel input output

---
+++ 13_File_IO.py
@@ -0,0 +1,22 @@
+"""
+File 
+
+    File Opeations
+        1. open("data.txt", "r")
+                file_name ,  modes - { r - read , w - write}
+                
+                -After opening file it must be close
+                using close()
+            
+            
+            
+            
+            modes : 
+            r - reading mode ---By default 
+            w - writing mode -- override and add new changes 
+            a - writing mode -- but it not override ( it add text at the end )
+            x - create a new / open for writing 
+            b - binary mode
+            t - text mode {default }
+            + - opens disk file for update (r & w)
+"""
