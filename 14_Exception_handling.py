ba4af204e1102f7e1fccc9d76210fc00d67f8e21
Prajwal <prajwalpidekar233@gmail.com>
2026-08-29 13:01:32 +0530

    exception handling

---
+++ 14_Exception_handling.py
@@ -0,0 +1,33 @@
+"""
+EXception Handling
+    - Try , except, else , finally 
+    
+    
+    Python built in exception
+
+"""
+
+
+
+
+"""
+List comprehensions
+    [ output for item in terable if condition]
+    
+    EX: sq =  [ i*i for i in range (6)]
+    print(sq)    :output - print 1 to  6 numbers squares
+    
+    # only odd numbers 
+    EX: sq =  [ i*i for i in range (6) if i%2 != 0]
+"""
+
+"""
+json module
+
+        1 . json.loads()
+        2 . json.dumps()
+        
+        1 . json.load()
+        2 . json.dump()
+
+"""
